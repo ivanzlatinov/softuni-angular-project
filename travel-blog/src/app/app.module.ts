b5adfea2606130bd1ab79ddb1a0dd48ee21df2b7
ivanivan <izlatinov19@gmail.com>
2023-08-02 02:38:41 +0300

    routing

--- travel-blog/src/app/app.module.ts
+++ travel-blog/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { UserService } from './user.service';
 import { storageServiceProvider } from './storage.service';
+import { CoreModule } from './core/core.module';
 
 
 
@@ -16,15 +15,13 @@ import { storageServiceProvider } from './storage.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
     AboutComponent,
-    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
   ],
   providers: [
     UserService,
--- travel-blog/src/app/pages/about/about.component.ts
+++ travel-blog/src/app/pages/about/about.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('About Us');
+  }
 
 }
